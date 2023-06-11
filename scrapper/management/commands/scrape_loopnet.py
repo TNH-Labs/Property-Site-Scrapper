f81092a4dd85598389d4acaa7dff40ce5784c236
iamumarjaved <81705788+iamumarjaved@users.noreply.github.com>
2023-06-11 20:11:03 +0500

    added requirements.txt file

--- scrapper/management/commands/scrape_loopnet.py
+++ scrapper/management/commands/scrape_loopnet.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
-from scrapper.spiders.loopnet_spider import LoopNetSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from scrapper.spiders.loopnet_spider import LoopNetSpider
 
 class Command(BaseCommand):
     help = 'Scrape data from LoopNet'
